31131160b4f8f7059c67c5508e9a80591e6d245c
Турмахан Динислам Даулетулы <@corp.itstep>
2019-05-14 16:49:10 +0600

    Release

---
+++ DocumentsLesson/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DocumentsLesson
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void PrintClick(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                printDialog.PrintVisual(textBlock, "Распечатываем элемент");
+            }
+        }
+    }
+}
